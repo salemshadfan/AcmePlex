26b833465bc90323351c7dd07f36e441e045f3ff
salemshadfan <144166796+salemshadfan@users.noreply.github.com>
2024-11-19 21:02:10 -0700

    Add files via upload

---
+++ src/main/java/com/acmeplex/acmeplex/AcmePlexApplication.java
@@ -0,0 +1,13 @@
+package com.acmeplex.acmeplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmePlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmePlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acmeplex/acmeplex/AcmePlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmeplex.acmeplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmePlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
